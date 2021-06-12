76089ef59d300e96fde5fbe0b2a2ce35a530bcd3
Ceci Cardenas <ceciliacrdz@hotmail.com>
2021-06-12 12:39:42 -0500

    Update sort by selection.py

--- sort by selection.py
+++ sort by selection.py
@@ -1,31 +1,17 @@
-'''
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-'''
-
-lista = [23,78,45,8,32,56]
+lista = [5,3,6,4,1,2,200,1000,890321,-20]
 
 menor = min(lista)
 
 pos=lista.index(min(lista))
-print(pos)
+#print(pos)
 i=0
 for num in lista:
     lista[pos]=lista[i]
     lista[i]=menor
-    print(lista)
+    #print(lista)
     i=i+1
     if i==(len(lista)):
         break
     menor=min(lista[i:len(lista)])
     pos=lista.index(menor)
-print(lista)
-        
-
-        
-    
-        
+print(lista)     
